82da576d74117bc3423c629abd6aac151b7ad49a
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2021-10-28 20:26:03 -0500

    added initial promt

--- Develop/script.js
+++ Develop/script.js
@@ -6,10 +6,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  window.alert("Please answer a few questions to generate your password");
+  
 
-  passwordText.value = password;
+
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+  // passwordText.value = password;
 
 }
 
